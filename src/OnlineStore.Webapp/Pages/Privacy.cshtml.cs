0abcee345696b8e837f9e80b2d2ad70b261b0f79
alexsandev <alex.rsjr10@gmail.com>
2026-09-06 20:26:05 -0300

    chore: inicializa a estrutura base do projeto

---
+++ src/OnlineStore.Webapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineStore.Webapp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
