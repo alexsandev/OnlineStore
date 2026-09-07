becc19eedf967859cae8da89dec06414c097fbd6
alexsandev <alex.rsjr10@gmail.com>
2026-09-07 20:40:37 -0300

    feat(domain): adiciona interface de repositório genérico e paginação

---
+++ src/OnlineStore.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.Domain.Pagination;
+
+namespace OnlineStore.Domain.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        Task<T?> GetAsync(long id, CancellationToken ct = default);
+        Task<PageResult<T>> GetPageAsync(PageRequest pr, CancellationToken ct = default);
+        Task AddAsync(T entity, CancellationToken ct = default);
+        Task UpdateAsync(T entity, CancellationToken ct = default);
+        Task DeleteAsync(long id, CancellationToken ct = default);
+    }
+}
