af1f0d282bae1c47e7ccca01102b395499ef0d33
alexsandev <alex.rsjr10@gmail.com>
2026-09-07 21:25:56 -0300

    feat(infra): implementa repositório genérico

--- src/OnlineStore.Domain/Interfaces/IRepository.cs
+++ src/OnlineStore.Domain/Interfaces/IRepository.cs
@@ -2,7 +2,7 @@
 
 namespace OnlineStore.Domain.Interfaces
 {
-    interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         Task<T?> GetAsync(long id, CancellationToken ct = default);
         Task<PageResult<T>> GetPageAsync(PageRequest pr, CancellationToken ct = default);
