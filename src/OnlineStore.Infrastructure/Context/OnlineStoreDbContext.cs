702e76fa051444971f4d5ab7c17aa6a56144c7b5
alexsandev <alex.rsjr10@gmail.com>
2026-09-13 14:43:41 -0300

    feat(infra): mapeia novas entidades no dbcontext e configura model builder

--- src/OnlineStore.Infrastructure/Context/OnlineStoreDbContext.cs
+++ src/OnlineStore.Infrastructure/Context/OnlineStoreDbContext.cs
@@ -6,7 +6,16 @@ namespace OnlineStore.Infrastructure.Context
     public class OnlineStoreDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
 
-        OnlineStoreDbContext(DbContextOptions<OnlineStoreDbContext> options) : base(options) { }
+        public OnlineStoreDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ProductCategory>().HasKey(pc => new { pc.ProductId, pc.CategoryId });
+        }
     }
 }
+
