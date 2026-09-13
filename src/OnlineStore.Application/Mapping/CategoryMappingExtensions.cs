031b98ca53ed4eaf72440217e4418ac826ada840
alexsandev <alex.rsjr10@gmail.com>
2026-09-12 21:21:35 -0300

    feat(product): adiciona dtos e mapping

---
+++ src/OnlineStore.Application/Mapping/CategoryMappingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Mapping
+{
+    public static class CategoryMappingExtensions
+    {
+        public static CategoryDto ToCategoryDto(this Category category)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description,
+                ImageUrl = category.ImageUrl
+            };
+        }
+    }
+}
---
+++ src/OnlineStore.Application/Mapping/ProductMappingExtensions.cs
@@ -0,0 +1,38 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Mapping
+{
+    public static class ProductMappingExtensions
+    {
+        public static ProductDto ToProductDto(this Product product)
+        {
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                ShortDescription = product.ShortDescription,
+                Description = product.Description,
+                Price = product.Price,
+                Stock = product.Stock,
+                MainImageUrl = product.MainImageUrl,
+                ImagesUrls = product.ImagesUrl.ToList(),
+                Categories = product.ProductCategories.Select(x => x.Category!.ToCategoryDto()).ToList()
+            };
+        }
+
+        public static Product ToProduct(this ProductDto productDto)
+        {
+            var product = new Product(
+                productDto.Name,
+                productDto.ShortDescription,
+                productDto.Description,
+                productDto.MainImageUrl,
+                productDto.Price,
+                productDto.Stock
+            );
+
+            return product;
+        }
+    }
+}
