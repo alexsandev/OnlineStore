2451e5515b9db22118a03b1109b2b283147b3fc9
alexsandev <alex.rsjr10@gmail.com>
2026-09-12 21:23:19 -0300

    feat(product): adiciona serviço de cadastro

---
+++ src/OnlineStore.Application/Interfaces/IProductAppService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.Application.DTOs;
+
+namespace OnlineStore.Application.Interfaces
+{
+    public interface IProductAppService
+    {
+        Task<ProductDto> RegisterProductAsync(ProductDto createProductDto, CancellationToken ct);
+    }
+}
---
+++ src/OnlineStore.Application/Services/ProductAppService.cs
@@ -0,0 +1,25 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Application.Interfaces;
+using OnlineStore.Application.Mapping;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Services
+{
+    internal class ProductAppService : IProductAppService
+    {
+        private readonly IRepository<Product> _repository;
+
+        public ProductAppService(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProductDto> RegisterProductAsync(ProductDto productDto, CancellationToken ct)
+        {
+            var product = productDto.ToProduct();
+            await _repository.AddAsync(product, ct);
+            return product.ToProductDto();
+        }
+    }
+}
